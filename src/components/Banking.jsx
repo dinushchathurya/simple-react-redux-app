8be30da4616f366fb1436a34e7f83c22a1157aad
dinushchathurya <dinushchathurya21@gmail.com>
2020-12-29 12:34:47 +0530

    :sparkles: Finish Banking.jsx

--- src/components/Banking.jsx
+++ src/components/Banking.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React , { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { deposit, withdraw, collectInterest, deleteAccount } from '../actions/bankingAction';
+
+import styles from './styles.css';
 
 const Banking = () => {
+
+    const [amount, setAmount] = useState("");
+    const dispatch = useDispatch();
+
+    const handleDeposit = () => {
+        dispatch(deposit(amount));
+    };
+    const handleWithdraw = () => {
+        dispatch(withdraw(amount));
+    };
+    const handleInterest = () => {
+        dispatch(collectInterest());
+    };
+    const handleDelete = () => {
+        dispatch(deleteAccount());
+    };
+
     return (
-        <div>
-            <h2>Banking</h2>
+        <div className="form-group">
+            <input value={amount} onChange={(e) => setAmount(e.target.value)} type="text" className="form-control" />
+            <div className="control_buttons">
+                <button onClick={handleDeposit} className="button btn btn-success">Deposit</button>
+                <button onClick={handleWithdraw} className="button btn btn-primary">Withdraw</button>
+                <button onClick={handleInterest} className="button btn btn-warning">Collect Interest</button>
+                <button onClick={handleDelete} className="button btn btn-danger">Delete Account</button>
+            </div>
         </div>
     )
 }
