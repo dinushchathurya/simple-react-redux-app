5a56abe3ebb6d5a48d9d5bf096358fe568bbad45
dinushchathurya <dinushchathurya21@gmail.com>
2020-12-29 12:34:11 +0530

    :sparkles: Finish Auth.jsx

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import styles from './styles.css';
+
 const Auth = () => {
     return (
-        <div>
-            <div className="btn btn-info">Login</div>
+        <div className="main_header">
+            <h1>Simple Banking App</h1>
         </div>
     )
 }
