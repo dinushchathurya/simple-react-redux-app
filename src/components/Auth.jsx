ca70e0c4c8dc7a80585c6810521cf3649a5b66dd
dinushchathurya <dinushchathurya21@gmail.com>
2020-12-29 09:48:10 +0530

    :sparkles: Create Auth component

---
+++ src/components/Auth.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Auth = () => {
+    return (
+        <div>
+            <div className="btn btn-info">Login</div>
+        </div>
+    )
+}
+
+export default Auth;
