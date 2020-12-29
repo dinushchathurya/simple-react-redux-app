be9c6ef1940073d306c818b66b87d3ffd1887216
dinushchathurya <dinushchathurya21@gmail.com>
2020-12-29 09:52:40 +0530

    :hammer:  Peering all the component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Auth from './components/Auth';
+import Balance from './components/Balance';
+import Banking from './components/Banking';
+import AccountStatus from './components/AccountStatus';
 
 const  App = () => {
     return (
         <div>
-            
+           <Auth /> 
+           <Balance />
+           <Banking />
+           <AccountStatus /> 
         </div>
     )
 }
