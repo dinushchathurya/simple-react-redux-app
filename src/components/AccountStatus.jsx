9c596c6d6092eb1f38f60c8fe0d6b418fbbf27e8
dinushchathurya <dinushchathurya21@gmail.com>
2020-12-29 09:51:53 +0530

    :sparkles: Create AccountState component

---
+++ src/components/AccountStatus.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AccountStatus = () => {
+    return (
+        <div>
+            <h2>AccountStatus</h2>
+        </div>
+    )
+}
+
+export default AccountStatus;
